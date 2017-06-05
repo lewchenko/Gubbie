426993b309024131c5d7bb8d6be5b084b0d3709b
lewchenko <lewchenko@gmail.com>
2017-06-05 06:09:58 -0700

    initial project version

---
+++ Gubbie.py
@@ -0,0 +1,10 @@
+# Gubbie Simulator - June 2017
+# This project will simulate "Gubbie" , an elderly dog with Diabetes, Cushings, Blind and Gall Bladder Removed
+# The purpose of the simulation will be to act as a reference for the real dog Gubbie
+# in order to assist with dosage predictability
+
+
+
+import sys
+string=sys.argv[1]
+print string [::-1]
